ee616c48fa812e539d44d2a52183f50b37a45a10
kristyyip <kristyyip51998@gmail.com>
2024-08-02 19:23:03 -0400

    defined Thought model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dayjs = require("dayjs");
+
+// Schema to create User model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: timestamp => dayjs(timestamp).format("MM/DD/YYY, h:mm:ss")
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+        virtuals: true,
+        getters: true,
+    },
+  }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        return this.reactions.length();
+    })
+
+const User = model('user', thoughtSchema);
+
+module.exports = User;
