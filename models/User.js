f6dfd8aa5a53604c5c20eec921e933f6b67b458b
kristyyip <kristyyip51998@gmail.com>
2024-08-02 19:14:01 -0400

    defined User model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        // validate email address
+        // src: https://stackoverflow.com/questions/18022365/mongoose-validate-email-syntax
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+    ]
+  },
+  {
+    toJSON: {
+        virtuals: true,
+    },
+  }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length();
+    })
+
+const User = model('user', userSchema);
+
+module.exports = User;
